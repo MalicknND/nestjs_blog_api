88b58a9133ae4fc0d98744c6e380325c3cd064c2
MalicknND <ndiayemalick062@gmail.com>
2026-01-13 13:47:24 +0100

    Les Décorateurs de Méthodes HTTP

--- src/posts/posts.controller.ts
+++ src/posts/posts.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post } from '@nestjs/common';
 import { PostsService } from './posts.service';
 
 @Controller('posts')
@@ -9,5 +9,17 @@ export class PostsController {
   @Get()
   findAll() {
     return this.postsService.findAll();
+  }
+
+  @Get(':id') // Correspond à GET /posts/1, GET /posts/abc, etc.
+  findOne(@Param('id') id: string) {
+    // Récupère le paramètre 'id' de l'URL
+    return `This action returns a #${id} post`;
+  }
+
+  @Post()
+  create(@Body() createPostDto: any) {
+    console.log(createPostDto);
+    return 'This action adds a new post';
   }
 }

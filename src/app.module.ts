7881dc33413f7f9ea0bf8b4aeb5a1f555a55506a
MalicknND <ndiayemalick062@gmail.com>
2026-01-13 14:11:39 +0100

    feat(database): integrate TypeORM with PostgreSQL for data management
    - Added TypeORM and PostgreSQL dependencies to package.json
    - Configured TypeORM in app.module.ts to connect to a PostgreSQL database
    - Set up database connection parameters including host, port, username, password, and database name
    - Enabled automatic synchronization of entities

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,23 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PostsModule } from './posts/posts.module';
 import { CategoriesModule } from './categories/categories.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [PostsModule, CategoriesModule],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'admin',
+      password: 'mysecretpassword',
+      database: 'blogapi',
+      entities: [],
+      synchronize: true,
+    }),
+    PostsModule,
+    CategoriesModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
